0d0185c7d540f90e9c2e5d1da1f59cb8a1537e61
Adam Hammes <ahammes@cortexmedia.ca>
2019-06-09 12:34:11 -0400

    Allow user-supplied stylesheets
    Most of the work came from me wanting to support live reload on the
    stylesheet file, which necessitated the refactor from `print_html` ->
    `watch_and_parse`.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ extern crate structopt;
 use pulldown_cmark::{html, Parser};
 use std::path::PathBuf;
 use std::sync::mpsc;
-use std::sync::mpsc::Receiver;
 use std::thread;
 
 use serde::Serialize;
@@ -18,11 +17,13 @@ use structopt::StructOpt;
 
 const DEFAULT_STYLES: &str = include_str!("default.css");
 
-#[derive(Debug, StructOpt)]
+#[derive(Clone, Debug, StructOpt)]
 #[structopt(name = "marp")]
 struct Cli {
     #[structopt(parse(from_os_str))]
     file: PathBuf,
+    #[structopt(short = "s", long = "stylesheet", parse(from_os_str))]
+    stylesheet: Option<PathBuf>,
 }
 
 #[derive(Debug, Serialize)]
@@ -33,25 +34,32 @@ struct Update {
 
 fn main() {
     let opt = Cli::from_args();
-    run(&opt.file);
+    run(opt);
 }
 
-fn run(input: &PathBuf) {
-    let (file_sender, file_receiver) = mpsc::channel();
+fn run(opt: Cli) {
+    let input = &opt.file;
 
-    let mut watcher = watcher(file_sender, Duration::from_millis(30)).unwrap();
-    watcher.watch(input, RecursiveMode::NonRecursive).unwrap();
 
+    let styles = if let Some(stylesheet_path) = &opt.stylesheet {
+        std::fs::read_to_string(&stylesheet_path).expect("could not read file")
+    } else {
+        DEFAULT_STYLES.to_string()
+    };
+
+    let shared_styles = std::sync::Arc::new(styles);
     let initial_html = std::sync::Arc::new(parse_file(&input));
+
 
     let websocket = ws::Builder::new()
         .build(move |out: ws::Sender| {
-            let cloned = initial_html.clone();
+            let cloned_content = initial_html.clone();
+            let cloned_styles = shared_styles.clone();
 
             move |_| {
                 let initial_message = Update {
-                    content: Some(cloned.to_string()),
-                    stylesheet: Some(DEFAULT_STYLES.to_string()),
+                    content: Some(cloned_content.to_string()),
+                    stylesheet: Some(cloned_styles.to_string()),
                 };
 
                 let serialized = serde_json::to_string(&initial_message).unwrap();
@@ -64,8 +72,9 @@ fn run(input: &PathBuf) {
 
     let broadcaster = websocket.broadcaster();
 
+    let cli = opt.clone();
     let parser_thread = thread::spawn(move || {
-        print_html(file_receiver, broadcaster);
+        watch_and_parse(&cli, broadcaster);
     });
 
     thread::spawn(move || websocket.listen("127.0.0.1:3012"));
@@ -78,14 +87,42 @@ fn run(input: &PathBuf) {
     parser_thread.join().unwrap();
 }
 
-fn print_html(receiver: Receiver<DebouncedEvent>, output: Sender) {
+fn watch_and_parse(config: &Cli, output: Sender) {
+    let (sender, receiver) = mpsc::channel();
+
+    let mut watcher = watcher(sender, Duration::from_millis(30)).unwrap();
+    watcher
+        .watch(&config.file, RecursiveMode::NonRecursive)
+        .unwrap();
+
+    if let Some(stylesheet) = &config.stylesheet {
+        watcher
+            .watch(stylesheet, RecursiveMode::NonRecursive)
+            .unwrap();
+    }
+
+    let canonical_content_path = std::fs::canonicalize(&config.file).unwrap();
+    let canonical_stylesheet_path = config
+        .stylesheet
+        .as_ref()
+        .map(|p| std::fs::canonicalize(p).unwrap());
+
     loop {
         if let Ok(DebouncedEvent::Write(path)) = receiver.recv() {
-            let markdown = parse_file(&path).to_string();
-            let update = Update {
-                content: Some(markdown),
-                stylesheet: None,
+            let (content, stylesheet) = if path == canonical_content_path {
+                (Some(parse_file(&path).to_string()), None)
+            } else if Some(&path) == canonical_stylesheet_path.as_ref() {
+                let styles = std::fs::read_to_string(&path).expect("could not read file");
+                (None, Some(styles))
+            } else {
+                unreachable!();
             };
+
+            let update = Update {
+                content,
+                stylesheet,
+            };
+
             let serialized = serde_json::to_string(&update).unwrap();
             output.send(serialized).unwrap();
         }
